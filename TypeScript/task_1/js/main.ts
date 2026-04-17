163b5ba53ecfad967f58899232c5dcb07c104865
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-17 10:34:05 +0300

    Task 3: printTeacher function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,13 +11,13 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return firstName[0] + '. ' + lastName;
 };
 
-console.log(director1);
+console.log(printTeacher('John', 'Doe'));
 
